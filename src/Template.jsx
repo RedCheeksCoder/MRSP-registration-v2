6476b87e84b01eb21dbd10daa972d8e70129f801
Bryan Odiña <149803435+RedCheeksCoder@users.noreply.github.com>
2024-06-14 09:55:32 +0800

    Update Template.jsx

--- src/Template.jsx
+++ src/Template.jsx
@@ -1,13 +1,159 @@
-import React from 'react';
+// import React from 'react';
+// import styled from 'styled-components';
+
+// const Container = styled.div`
+//   margin:auto;
+//   position:relative;
+//   background-color: white;
+// `
+// const TemplateContainer = styled.div`
+// font-family:"Inter";
+//   width: 1280px;
+//   height: 550px;
+//   display: flex;
+//   flex-direction: row;
+//   align-items: center;
+//   justify-content: center;
+//   text-align: center;
+// `;
+
+// const Info = styled.div`
+//   text-align:left;
+//   margin-right:1rem;
+//   padding-left:2rem;
+// width:700px;
+// `
+
+// const Name = styled.h1`
+// font-family:"Inter";
+// box-sizing:border-box;
+// margin: 0;
+// max-width: 700px;
+// font-size: 4rem;
+// color: #000;
+// font-weight:bold;
+// line-height:1;
+// margin-bottom:1.5rem;
+// `;
+
+// const Details = styled.p`
+//   font-size: 2rem;
+//   font-weight:bold;
+//   color: #000;
+// `;
+
+// const Footer = styled.div`
+// font-family:"Inter";
+// text-align:center;
+// color: #fff;
+//   font-size: 3rem;
+//   margin:auto;
+//   background-color:#000;
+// `
+
+// const Role = styled.h1`
+//   font-size:5rem;
+//   font-weight:700;
+//   margin:0;
+// `
+// const Category = styled.h2`
+//   font-size:3rem;
+//   margin:0;
+// `
+// const Logo = styled.div`
+//   display:flex;
+//   flex-direction:row;
+//   margin:0;
+//   padding: 1rem 0 0 1rem;
+//   height:120px;
+//   width:130px;
+//   img{
+//   border:none;
+//   background-color:white;
+//   padding: 0 0.1rem;
+//   }
+// `
+
+// const DotsTop = styled.img`
+//   position:absolute;
+//   right:0;
+//   padding:0;
+// `
+// const DotsLeft = styled.img`
+//   position:absolute;
+//   left:0;
+//   padding:0;
+
+//   transform: rotate(180deg);
+// `
+
+// const CopyButton = styled.button`
+//   background: #007bff;
+//   border: none;
+//   color: white;
+//   border-radius: 5px;
+//   width: 250px;
+//   height: 70px;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   cursor: pointer;
+//   bottom:20px;
+// margin:auto;
+// margin-top:2rem;
+// font-size:2rem;
+
+//   &:hover {
+//     background: #0056b3;
+//   }
+// `;
+
+// const Template = ({ name, chapter, school, role, competitionCategory, joinAs, qrCode }) => {
+//   return (
+//     <>
+//     <Container>
+//       <Logo><img src="/logo1.png" alt="" />
+//       <img src="/logo2.png" alt="" />
+//       <img src="/logo3.png" alt="" />
+//       <img src="/logo4.png" alt="" />
+//       <img src="/logo5.png" alt="" />
+//       <img src="/logo6.png" alt="" />
+//       <img src="/logo7.png" alt="" />
+//       <DotsTop src="/topdots.jpg" alt="" height={200} width={200}/>
+//       </Logo>
+//     <TemplateContainer id="template">
+//       <Info>
+//         <DotsLeft src="/leftdots.jpg" alt="" height={200} width={100}/>
+//       <Name>{name}</Name>
+//       <Details>{school}</Details>
+//       <Details>{chapter}</Details>
+//       <Details>{role}</Details>
+//       </Info>
+//       {qrCode && <img src={qrCode} alt={`QR code for ${name}`} height={350} width={350}/>}
+//     </TemplateContainer>
+
+//     <Footer>
+//       <Role>{joinAs}</Role>
+//       <Category>{`(${competitionCategory})`}</Category>
+//     </Footer>
+//     </Container>
+//     </>
+//   );
+// };
+
+// export default Template;
+
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
   margin:auto;
   position:relative;
   background-color: white;
-`
+`;
+
 const TemplateContainer = styled.div`
-font-family:"Inter";
+  font-family: "Inter";
   width: 1280px;
   height: 550px;
   display: flex;
@@ -18,74 +164,76 @@ font-family:"Inter";
 `;
 
 const Info = styled.div`
-  text-align:left;
-  margin-right:1rem;
-  padding-left:2rem;
-width:700px;
-`
+  text-align: left;
+  margin-right: 1rem;
+  padding-left: 2rem;
+  width: 700px;
+`;
 
 const Name = styled.h1`
-font-family:"Inter";
-box-sizing:border-box;
-margin: 0;
-max-width: 700px;
-font-size: 4rem;
-color: #000;
-font-weight:bold;
-line-height:1;
-margin-bottom:1.5rem;
+  font-family: "Inter";
+  box-sizing: border-box;
+  margin: 0;
+  max-width: 700px;
+  font-size: 4rem;
+  color: #000;
+  font-weight: bold;
+  line-height: 1;
+  margin-bottom: 1.5rem;
 `;
 
 const Details = styled.p`
   font-size: 2rem;
-  font-weight:bold;
+  font-weight: bold;
   color: #000;
 `;
 
 const Footer = styled.div`
-font-family:"Inter";
-text-align:center;
-color: #fff;
+  font-family: "Inter";
+  text-align: center;
+  color: #fff;
   font-size: 3rem;
-  margin:auto;
-  background-color:#000;
-`
+  margin: auto;
+  background-color: #000;
+`;
 
 const Role = styled.h1`
-  font-size:5rem;
-  font-weight:700;
-  margin:0;
-`
+  font-size: 5rem;
+  font-weight: 700;
+  margin: 0;
+`;
+
 const Category = styled.h2`
-  font-size:3rem;
-  margin:0;
-`
+  font-size: 3rem;
+  margin: 0;
+`;
+
 const Logo = styled.div`
-  display:flex;
-  flex-direction:row;
-  margin:0;
+  display: flex;
+  flex-direction: row;
+  margin: 0;
   padding: 1rem 0 0 1rem;
-  height:120px;
-  width:130px;
-  img{
-  border:none;
-  background-color:white;
-  padding: 0 0.1rem;
+  height: 120px;
+  width: 130px;
+  img {
+    border: none;
+    background-color: white;
+    padding: 0 0.1rem;
   }
-`
+`;
 
 const DotsTop = styled.img`
-  position:absolute;
-  right:0;
-  padding:0;
-`
-const DotsLeft = styled.img`
-  position:absolute;
-  left:0;
-  padding:0;
+  position: absolute;
+  right: 0;
+  padding: 0;
+`;
 
+const DotsLeft = styled.img`
+  position: absolute;
+  left: 0;
+  padding: 0;
   transform: rotate(180deg);
-`
+`;
 
 const CopyButton = styled.button`
   background: #007bff;
@@ -99,49 +247,61 @@ const CopyButton = styled.button`
   justify-content: center;
   cursor: pointer;
   bottom:20px;
-margin:auto;
-margin-top:2rem;
-font-size:2rem;
+  margin:auto;
+  margin-top:2rem;
+  font-size:2rem;
 
   &:hover {
     background: #0056b3;
   }
 `;
 
-const Template = ({ name, chapter, school, role, competitionCategory, joinAs, qrCode }) => {
+const Template = ({ name, chapter, school, role, competitionCategory, joinAs, qrCode, onLoad }) => {
+  const [loadedImages, setLoadedImages] = useState(0);
+  const totalImages = 7; // Number of images to load
+
+  const handleImageLoad = () => {
+    setLoadedImages(prev => prev + 1);
+  };
+
+  useEffect(() => {
+    if (loadedImages === totalImages) {
+      onLoad();
+    }
+  }, [loadedImages, totalImages, onLoad]);
+
   return (
     <>
-    <Container>
-      <Logo><img src="/logo1.png" alt="" />
-      <img src="/logo2.png" alt="" />
-      <img src="/logo3.png" alt="" />
-      <img src="/logo4.png" alt="" />
-      <img src="/logo5.png" alt="" />
-      <img src="/logo6.png" alt="" />
-      <img src="/logo7.png" alt="" />
-      <DotsTop src="/topdots.jpg" alt="" height={200} width={200}/>
-      </Logo>
-    <TemplateContainer id="template">
-      <Info>
-        <DotsLeft src="/leftdots.jpg" alt="" height={200} width={100}/>
-      <Name>{name}</Name>
-      <Details>{school}</Details>
-      <Details>{chapter}</Details>
-      <Details>{role}</Details>
-      </Info>
-      {qrCode && <img src={qrCode} alt={`QR code for ${name}`} height={350} width={350}/>}
-    </TemplateContainer>
-
-    <Footer>
-      <Role>{joinAs}</Role>
-      <Category>{`(${competitionCategory})`}</Category>
-    </Footer>
-    </Container>
+      <Container>
+        <Logo>
+          <img src="/logo1.png" alt="" onLoad={handleImageLoad} />
+          <img src="/logo2.png" alt="" onLoad={handleImageLoad} />
+          <img src="/logo3.png" alt="" onLoad={handleImageLoad} />
+          <img src="/logo4.png" alt="" onLoad={handleImageLoad} />
+          <img src="/logo5.png" alt="" onLoad={handleImageLoad} />
+          <img src="/logo6.png" alt="" onLoad={handleImageLoad} />
+          <img src="/logo7.png" alt="" onLoad={handleImageLoad} />
+          <DotsTop src="/topdots.jpg" alt="" height={200} width={200} onLoad={handleImageLoad} />
+        </Logo>
+        <TemplateContainer id="template">
+          <Info>
+            <DotsLeft src="/leftdots.jpg" alt="" height={200} width={100} onLoad={handleImageLoad} />
+            <Name>{name}</Name>
+            <Details>{school}</Details>
+            <Details>{chapter}</Details>
+            <Details>{role}</Details>
+          </Info>
+          {qrCode && <img src={qrCode} alt={`QR code for ${name}`} height={350} width={350} />}
+        </TemplateContainer>
+        <Footer>
+          <Role>{joinAs}</Role>
+          <Category>{`(${competitionCategory})`}</Category>
+        </Footer>
+      </Container>
     </>
   );
 };
 
 export default Template;
-
 
 
