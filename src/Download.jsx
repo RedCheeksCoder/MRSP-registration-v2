045aed38caad3983b92853b2d9c8c6a48da69c90
Bryan Odiña <149803435+RedCheeksCoder@users.noreply.github.com>
2024-06-14 09:56:13 +0800

    Update Download.jsx

--- src/Download.jsx
+++ src/Download.jsx
@@ -1,3 +1,63 @@
+// import React, { useEffect, useRef, useState } from 'react';
+// import { toPng } from 'html-to-image';
+// import Template from './Template';
+// import styled from 'styled-components';
+// import QRCode from 'qrcode';
+
+// const Container = styled.div`
+//   width: fit-content;
+// `;
+
+// const Download = ({ participant }) => {
+//   const templateRef = useRef(null);
+//   const [qrCode, setQrCode] = useState('');
+
+//   const generateQrCode = async (url) => {
+//     const qrCodeUrl = await QRCode.toDataURL(url);
+//     setQrCode(qrCodeUrl);
+//   };
+
+//   useEffect(() => {
+//     const url = `https://mrsp-registration-v2.vercel.app/participant/info/${participant.id}`;
+//     generateQrCode(url);
+//   }, [participant.id]);
+
+//   const handleDownload = async () => {
+//     if (templateRef.current && qrCode) {
+//       const dataUrl = await toPng(templateRef.current);
+//       const link = document.createElement('a');
+//       link.href = dataUrl;
+//       link.download = `${participant.firstName}_${participant.lastName}.png`;
+//       link.click();
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (qrCode) {
+//       handleDownload();
+//     }
+//   }, [qrCode]);
+
+//   return (
+//     <Container ref={templateRef}>
+//       <div >
+//         <Template
+//           name={`${participant.firstName} ${participant.lastName}`}
+//           chapter={participant.chapter}
+//           school={participant.schoolCompany}
+//           role={participant.role}
+//           competitionCategory={participant.competitionCategory}
+//           joinAs={participant.nonParticipant}
+//           qrCode={qrCode} // Pass the QR code as a prop
+//         />
+//       </div>
+//     </Container>
+//   );
+// };
+
+// export default Download;
+
+
 import React, { useEffect, useRef, useState } from 'react';
 import { toPng } from 'html-to-image';
 import Template from './Template';
@@ -11,6 +71,7 @@ const Container = styled.div`
 const Download = ({ participant }) => {
   const templateRef = useRef(null);
   const [qrCode, setQrCode] = useState('');
+  const [imagesLoaded, setImagesLoaded] = useState(false);
 
   const generateQrCode = async (url) => {
     const qrCodeUrl = await QRCode.toDataURL(url);
@@ -28,19 +89,29 @@ const Download = ({ participant }) => {
       const link = document.createElement('a');
       link.href = dataUrl;
       link.download = `${participant.firstName}_${participant.lastName}.png`;
+
       link.click();
     }
   };
 
   useEffect(() => {
-    if (qrCode) {
-      handleDownload();
+    if (qrCode && imagesLoaded) {
+      const timeoutId = setTimeout(() => {
+        handleDownload();
+      }, 3000); // 3000 milliseconds = 3 seconds
+
+      // Clean up the timeout to prevent memory leaks
+      return () => clearTimeout(timeoutId);
     }
-  }, [qrCode]);
+  }, [qrCode, imagesLoaded]);
+
+  const handleImageLoad = () => {
+    setImagesLoaded(true);
+  };
 
   return (
-    <Container ref={templateRef}>
-      <div >
+    <Container>
+      <div ref={templateRef}>
         <Template
           name={`${participant.firstName} ${participant.lastName}`}
           chapter={participant.chapter}
@@ -48,7 +119,8 @@ const Download = ({ participant }) => {
           role={participant.role}
           competitionCategory={participant.competitionCategory}
           joinAs={participant.nonParticipant}
-          qrCode={qrCode} // Pass the QR code as a prop
+          qrCode={qrCode}
+          onLoad={handleImageLoad} // Pass onLoad to handle image load events
         />
       </div>
     </Container>
@@ -56,6 +128,5 @@ const Download = ({ participant }) => {
 };
 
 export default Download;
-
 
 
